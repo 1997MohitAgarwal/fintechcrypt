5bb829985664025ac0f3220f8903fb68e4684c0d
1997MohitAgarwal <152138497+1997MohitAgarwal@users.noreply.github.com>
2024-11-20 00:11:20 +0530

    spinner component changed

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { ClipLoader } from 'react-spinners';
+import { BeatLoader } from 'react-spinners';
 
 const Spinner = ({height}) => {
   return (
     <div className={`flex justify-center items-center ${height}`}>
-      <ClipLoader color="#ff6347" size={100} />
+      <BeatLoader color="#ff6347" size={50} />
     </div>
   );
 };
